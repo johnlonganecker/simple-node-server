e765f72ae0a091f934719b45fd81d8d5ffeb7fcd
john longanecker <johnlonganecker@gmail.com>
2018-12-05 15:47:10 -0800

    add in check for mysql and return hostname

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const http = require('http');
+const dns = require('dns');
 const port = process.env.PORT
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server!')
+
+  if (process.env.VCAP_SERVICES) {
+    let vcap_services = JSON.parse(process.env.VCAP_SERVICES),
+      host = vcap_services["p.mysql"][0]["credentials"]["hostname"];
+
+    dns.lookup(host, function(err, result) {
+      if(err) {
+        response.end('something bad happened - ' + err)
+        return console.log('something bad happened', err)
+      }
+      response.end(result)
+    })
+  }
+  else {
+    response.end('No MySQL service bound to app');
+  }
 }
 
 const server = http.createServer(requestHandler)
